64412b37efb40f1fc9bcbca63873cdb795d4d02c
Isai Sanzy <isai10sanchez@gmail.com>
2024-11-18 13:40:03 -0700

    created template files for every problem

---
+++ templates/main.cpp
@@ -0,0 +1,9 @@
+#include "solution.h"
+#include <iostream>
+
+int main() {
+  Solution solution;
+  std::cout << "Running test cases..." << std::endl;
+  std::cout << solution.exampleFunction() << std::endl;
+  return 0;
+}
---
+++ templates/solution.h
@@ -0,0 +1,11 @@
+#ifndef SOLUTION_H
+#define SOLUTION_H
+
+class Solution {
+public:
+  int exampleFunction() {
+    return 42; // Placeholder
+  }
+};
+
+#endif // SOLUTION_H
